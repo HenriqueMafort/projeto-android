3446a72bac746e5e6070ae1fdaf02e80b679484e
HenriqueMafort <hmafort@gmail.com>
2022-09-23 12:01:15 -0300

    .

--- calc-FGTS/js/script.js
+++ calc-FGTS/js/script.js
@@ -1,26 +1,27 @@
 function Calcular(){
-    var valor = window.document.getElementById('valorfgts')
+    var valor = window.document.getElementById('valorfgts').value
     var res = window.document.getElementById('res')
-    var percentual = 0.2
-    var  newfgts = (valor*percentual)
+    var percentual = Number(0.2)
+    var  newfgts = Number(valor*percentual.value)
     
-
-    if (valor.value.length == 0){
-        window.alert('Você precisa colocar algum valor no campo')
-    }else if ((valor > 0) && (valor <= 500)){
-        window.alert(`Você recebera R$500,00 reais`)
-    }if ((valor > 500) && (valor < 1000)){
-        window.alert(`${valor*0.4 + 50} Reais`)
-    }if((valor > 1000.01) && (valor <= 5000)){
-        window.alert(`você receberá ${valor * 0.3 + 150} Reais`)
-    }if ((valor > 5000.01) && (valor <= 10000.00 )){
-        window.alert(`Você receberá ${valor * 0.2 + 650} Reais`)
-        res.innerHTML = `Você tem ${valor * 0.2 + 650}`
-    }if((valor > 10000.01) && (valor < 15000)){
-        window.alert(`Você receberá R$ ${valor * 0.15 + 1150.50} Reais`)
-    }if((valor > 15000.01) && (valor < 20000)){
-        window.alert(`Você receberá ${valor * 0.1 + 1900} Reais`)
-    }else if(valor > 20000.01){
-        window.alert(`Você receberá ${valor * 0.05 + 2900} Reais`) 
+    valor.innerHTML = ''
+    res.innerHTML = 'Preencha os valores'
+    if(valor.length == 0){
+        alert('Preencha os dados para verificar os valores')
+    }else if (valor < 500){
+        window.alert(`Você não tem direito a saque aniversário`)
+    }if (valor > 500 && valor < 1000){
+       res.innerHTML = (`Você receberá o valor de R$${(valor*0.4 + 50).toFixed(2)} Reais`)
+    }if(valor > 1000.01 && valor <= 5000){
+      res.innerHTML = (`Você receberá o valor de R$ ${(valor*0.3 + 150).toFixed(2)} Reais`)
+    }if (valor > 5000.01 && valor <= 10000.00 ){
+        res.innerHTML = `Você receberá o valor de R$${(valor * 0.2 + 650).toFixed(2)}`
+    }if(valor > 10000.01 && valor < 15000){
+       res.innerHTML = (`Você receberá o valor de R$ ${(valor * 0.15 + 1150.50).toFixed(2)} Reais`)
+    }if(valor > 15000.01 && valor < 20000){
+       res.innerHTML = (`Você receberá o valor de ${(valor * 0.1 + 1900).toFixed(2)} Reais`)
+    } if(valor > 20000.01){
+        res.innerHTML = `Você receberá o valor de R$${valor * 0.05 + 2900}`
     }
+    
 }
